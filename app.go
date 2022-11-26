dfb95b2faf1c671453e75b47e49536bfefe63474
OlegGulevskyy <oleggulevskyy@gmail.com>
2022-11-26 16:54:01 +0100

    add global shortcut usage

--- app.go
+++ app.go
@@ -3,6 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"golang.design/x/hotkey"
+	"golang.design/x/hotkey/mainthread"
 )
 
 // App struct
@@ -19,9 +24,46 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	// register hotkey on the app startup
+	// if you try to register it anywhere earlier - the app will hang on compile step
+	mainthread.Init(a.RegisterHotKey)
 }
 
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// just a wrapper to have access to App functions
+// not necessary if you don't plan to do anything with your App on shortcut use
+func (a *App) RegisterHotKey() {
+	registerHotkey(a)
+}
+
+func registerHotkey(a *App) {
+	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
+	err := hk.Register()
+	if err != nil {
+		return
+	}
+
+	// you have 2 events available - Keyup and Keydown
+	// you can either or neither, or both
+	fmt.Printf("hotkey: %v is registered\n", hk)
+	<-hk.Keydown()
+	// do anything you want on Key down event
+	fmt.Printf("hotkey: %v is down\n", hk)
+
+	<-hk.Keyup()
+	// do anything you want on Key up event
+	fmt.Printf("hotkey: %v is up\n", hk)
+
+	runtime.EventsEmit(a.ctx, "Backend:GlobalHotkeyEvent", time.Now().String())
+
+	hk.Unregister()
+	fmt.Printf("hotkey: %v is unregistered\n", hk)
+
+	// reattach listener
+	registerHotkey(a)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,63 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useEffect, useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
+import { EventsOn } from "../wailsjs/runtime/runtime";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+  const [currentTime, setCurrentTime] = useState<string | null>(null);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function globalHotkeyEventHandler(time: string) {
+    setCurrentTime(time);
+  }
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+  useEffect(() => {
+    EventsOn("Backend:GlobalHotkeyEvent", globalHotkeyEventHandler);
+  }, []);
+
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+
+  const time =
+    currentTime ||
+    "Use keyboard shortcut Ctrl + Shift + S to find out current time";
+
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={greet}>
+          Greet
+        </button>
+        <div>
+          Current time from backend:
+          <p>{time}</p>
         </div>
-    )
+        <p className="disclaimer">
+          This keyboard shortcut works anywhere on your PC, as long as the app
+          process is running, even if the window is not focused.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
